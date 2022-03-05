b41c1e99b85a5c01f956c152a9c9f7bef1511e24
georgy1705 <shimgk1705@gmail.com>
2022-03-05 11:13:43 +0300

    Add modal window for change data

--- js/getData.js
+++ js/getData.js
@@ -1,40 +1,42 @@
 const getData = () => {
+    const changeBtn = document.querySelector('.btn-save')
+
     const renderCar = (cars) => {
         const carsContainer = document.querySelector('.cars-list')
 
+        const carCard = document.createElement('div')
+
+        carCard.className = 'cars-card'
+
         carsContainer.innerHTML = ""
 
-        console.log(cars);
-
         for (key in cars) {
-            const carBlock = document.createElement('div')
-            const carGarage = document.createElement('div')
-
             if (key === 'Item') {
-                carBlock.innerHTML = `
-                    <div class="cars-card">
-                        <div class="class-card__item">
+                carCard.innerHTML = `
+                        <div class="cars-card__item">
                             <h3 class="cars-title">${cars[key].Title}</h3>
                             <p class="cars-description">${cars[key].Original.CarOptions.Title}</p>
                             <p class="cars-description">${cars[key].Description}</p>
                             <ul>
                                 <li>Fuel type: ${cars[key].KeyValues.FuelType}</li>
                                 <li>Trim level: ${cars[key].KeyValues.TrimLevel}</li>
-                                <li>Gear box ${cars[key].KeyValues.GearBox}</li>
+                                <li>Gear box: ${cars[key].KeyValues.GearBox}</li>
                             </ul>
                         </div>
-                    </div>
                 `
             } else if (key === 'Garage') {
-                carGarage.innerHTML = `
-                    <div class="class-card__garage">
-                        <h3></h3>
+                carCard.insertAdjacentHTML("beforeEnd", `
+                    <div class="cars-card__garage">
+                        <p class="card-info">Email: ${cars[key].Email}</p>
+                        <p class="card-info">Name: ${cars[key].Name}</p>
+                        <p class="card-info">Owner: ${cars[key].Owner}</p>
+                        <btn class="btn btn-primary" data-toggle="modal" data-target="#modal">Change data</btn>
                     </div>
-                `
+                `)
             }
-
-            carsContainer.append(carBlock)
         }
+
+        carsContainer.append(carCard)
     }
 
     const getCars = () => {
@@ -42,14 +44,41 @@ const getData = () => {
             .then(response => response.json())
             .then(data => {
                 localStorage.setItem('cars', JSON.stringify(data))
-
-                renderCar(data)
             })
     }
 
-    // if (localStorage.getItem('cars')) {
-    //     renderCar(JSON.parse(localStorage.getItem('cars')))
-    // }
+    changeBtn.addEventListener('click', () => {
+        let cars = JSON.parse(localStorage.getItem('cars'))
+        const modal = document.querySelector('.modal')
+        const labels = modal.querySelectorAll('.modal__label')
+
+        const sendMessage = {}
+
+        labels.forEach(label => {
+            const span = label.querySelector('span')
+            const input = label.querySelector('input')
+
+            sendMessage[span.textContent] = input.value
+        })
+
+        let changeCars = {...cars}
+
+        for (key in changeCars) {
+            if (key === 'Garage') {
+                changeCars[key].Email = sendMessage['Email']
+                changeCars[key].Name = sendMessage['Name']
+                changeCars[key].Owner = sendMessage['Owner']
+            }
+        }
+
+        localStorage.setItem('cars', JSON.stringify(changeCars))
+        
+        renderCar(changeCars)
+    })
+
+    if (localStorage.getItem('cars')) {
+        renderCar(JSON.parse(localStorage.getItem('cars')))
+    }
 
     getCars()
 }
